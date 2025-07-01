6f3470c26e3ee08bef40da0a3e5b5dfd2b049501
trighele <trighele@gmail.com>
2025-07-01 10:46:29 -0400

    test deployment

--- src/components/pages/AboutMe/AboutMePage.jsx
+++ src/components/pages/AboutMe/AboutMePage.jsx
@@ -10,7 +10,7 @@ export default function AboutMePage() {
         <div className="content">
           <p>Hello! I'm Tom Righele, a Senior Application Developer at Marsh & McLennan with over four years of experience in app development, integration, and project leadership. My passion for technology runs deep, spanning backend development, website creation, and hardware—I've built multiple computers for family and friends.</p>
           <p>I'm 38 years old, married to my wonderful wife, and we recently welcomed our newborn son, Tommy. We share our home with our beloved pit bull, Georgie. As a die-hard New York Yankees fan, Mariano Rivera remains my all-time favorite player, and I take pride in being a multi-time fantasy football champion.</p>
-          <p>Currently, I'm actively seeking new projects and outside work opportunities. I love connecting with new people, sharing ideas, and exploring innovative collaborations. In my free time, I enjoy working out, spending quality time with family, and occasionally unwinding with a glass of tequila. I embrace life's dynamic aspects.</p>
+          <p>Currently, I'm actively seeking new projects and outside work opportunities! I love connecting with new people, sharing ideas, and exploring innovative collaborations. In my free time, I enjoy working out, spending quality time with family, and occasionally unwinding with a glass of tequila. I embrace life's dynamic aspects.</p>
         </div>
       </div>
       <div className="second-row">
